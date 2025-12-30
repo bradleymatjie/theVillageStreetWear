02f5c5d53619b92cb804d767c584457f4b0871b9
Bradley Matjie <bradleysaint45@gmail.com>
2025-12-30 11:58:56 +0200

    removed countdown

--- app/layout.tsx
+++ app/layout.tsx
@@ -86,7 +86,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${poppins.variable} antialiased`}>
-        <div className="pt-12">
+        <div>
           <Header />
           {children}
           <Footer />
